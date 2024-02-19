49274450b576403305e61472568f7623c7af4ca8
viktorsynek <ktor.syn@gmail.com>
2024-02-19 17:19:47 +0100

    todos controller for handling all REST request methods

---
+++ server/controllers/todos.js
@@ -0,0 +1,19 @@
+exports.getTodos = (req,res,next) => {
+    res.status(200);
+}
+
+exports.getTodo = (req,res,next) => {
+    res.status(200);
+}
+
+exports.createTodo = (req,res,next) => {
+    res.status(200);
+}
+
+exports.updateTodo = (req,res,next) => {
+    res.status(200);
+}
+
+exports.deleteTodo = (req,res,next) => {
+    res.status(200);
+}
