5836ce470d4b190e5e68ac82ca2f2faa0536cd0c
viktorsynek <ktor.syn@gmail.com>
2024-02-20 09:28:00 +0100

    create basic navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+    return ( 
+    <>
+    <div className="topnav">
+        <div className="flex">
+            <ul className='topnav-items'>
+                <li><Link to="/" id='title'>vTodo</Link></li>
+            </ul>
+            <ul className='topnav-items'>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Signup</Link></li>
+            </ul>
+        </div>
+    </div>
+    </>
+    );
+}
+ 
+export default Navbar;
