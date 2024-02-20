4716ece33f1ca96ecb1eebcfda2c25c847125d80
viktorsynek <ktor.syn@gmail.com>
2024-02-20 09:28:25 +0100

    create home page, if logged in

---
+++ client/src/components/Todos.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+
+const Todos = () => {
+    return ( 
+        <>
+            <div className='box'>
+                <button>Add Task</button>
+            </div>
+        </>
+     );
+}
+ 
+export default Todos;
+
