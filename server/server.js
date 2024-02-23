3e95bc247a7f9a26908457bebb39a79e10b0ac75
viktorsynek <ktor.syn@gmail.com>
2024-02-23 14:26:31 +0100

    add console connection

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const todos = require('./routes/todo');
 const logger = require('./middleware/logger');
+const connectDB = require('./config/db');
 
 dotenv.config({path: './config/config.env'});
+
+connectDB();
 
 const app = express();
 
