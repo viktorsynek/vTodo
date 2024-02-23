9400da86497b34061deaa3a8d22a8c2d21340c4a
viktorsynek <ktor.syn@gmail.com>
2024-02-23 14:27:08 +0100

    create logic for database connection

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(
+      process.env.DB_URI  
+    );
+
+    console.log(`Database is connected: ${conn.connection.host}`.cyan);
+}
+
+module.exports = connectDB;
