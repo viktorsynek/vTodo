a49c7e61482a710760449fc13ddc4651bc14e011
viktorsynek <ktor.syn@gmail.com>
2024-02-23 15:05:04 +0100

    handle REST methods for todos

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,19 +1,78 @@
-exports.getTodos = (req,res,next) => {
-    res.status(200);
+const Todo = require('../models/Todo');
+
+exports.getTodos = async (req,res,next) => {
+    try {
+        const Todo = await Todo.find();
+
+        res.status(200).json({
+            success: true,
+            count: Todo.length,
+            data: Todo
+        });
+    } catch (error) {
+        res.status(400).json({sucess:false});
+    }
 }
 
-exports.getTodo = (req,res,next) => {
-    res.status(200);
+exports.getTodo = async (req,res,next) => {
+    try {
+        const Todo = await Todo.findById(req.params.id);
+        if(!bootcamp){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({
+            success: true,
+            data: Todo
+        });
+    } catch (error) {
+        res.status(400).json({sucess:false});
+    }
 }
 
-exports.createTodo = (req,res,next) => {
-    res.status(200);
+exports.createTodo = async (req,res,next) => {
+    try {
+        const Todo = await Todo.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: Todo
+        });
+    } catch (error) {
+        res.status(400).json({sucess:false});
+    }
 }
 
-exports.updateTodo = (req,res,next) => {
-    res.status(200);
+exports.updateTodo = async (req,res,next) => {
+    try {
+        const Todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+            new:true,
+            runValidators: true
+        });
+
+        if(!bootcamp){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({
+            success: true,
+            data: Todo
+        });
+    } catch (error) {
+        res.status(400).json({sucess:false});
+    }
 }
 
-exports.deleteTodo = (req,res,next) => {
-    res.status(200);
+exports.deleteTodo = async (req,res,next) => {
+    try {
+        const Todo = await Todo.findByIdAndDelete(req.params.id);
+        
+        if(!bootcamp){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (error) {
+        res.status(400).json({sucess:false});
+    }
 }
