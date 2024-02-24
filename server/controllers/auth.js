87ac2ee73be23dc3161ad1a0d9b7a462f768c240
viktorsynek <ktor.syn@gmail.com>
2024-02-24 08:43:57 +0100

    create user register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,13 @@
 const User = require('../models/User');
 
 exports.register = async(req,res,next) => {
-    res.status(201).json({success: true});
+    const { username, email, password } = req.body;
+
+    const user = await User.create({
+        username,
+        email,
+        password
+    })
+
+    res.status(201).json({ success: true });
 };
