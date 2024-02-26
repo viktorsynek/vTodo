deab5b5ebafdb1cc2720823767a19c446cf26bfa
viktorsynek <ktor.syn@gmail.com>
2024-02-26 20:26:37 +0100

    create function to protect routes for authorized users only

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+
+exports.protect = async (req,res,next) => {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(' ')[1];
+    }
+    // else if(req.cookies.token)
+    // {
+    //     token = req.cookies.token;
+    // }
+    if (!token)
+    {
+        return next(new ErrorResponse('Not authorized to access this route', 401));
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+        req.user = await User.findById(decoded.id);
+        next();
+    } catch (error) {
+        return next(new ErrorResponse('Not authorized to access this route', 401));
+        
+    }
+};
