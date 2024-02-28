b73faa30bc50ccc11315837d7d633395c24846d5
viktorsynek <ktor.syn@gmail.com>
2024-02-28 22:00:02 +0100

    add cors support

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
