a1c30a231d0982cb4a0679bc54eaadd1fa9f2a0d
viktorsynek <ktor.syn@gmail.com>
2024-02-28 21:59:35 +0100

    fix typo

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, getMe, forgotPasswor, resetPasswordd} = require('../controllers/auth');
+const {register, login, getMe, forgotPassword, resetPassword} = require('../controllers/auth');
 const {protect} = require('../middleware/auth');
 
 const router = express.Router();
@@ -8,6 +8,6 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
-router.put('/resetpassword/:resettoken', resetPasswordd);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
