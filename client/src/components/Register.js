1970917f075569c18dda395f2e016fbb5c9053ab
viktorsynek <ktor.syn@gmail.com>
2024-02-28 22:02:17 +0100

    create register logic with fetching

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,21 +1,51 @@
 import Navbar from './Navbar';
 import '../css/reglog.css';
+import React, { useEffect } from 'react';
 
 const Register = () => {
-  return ( 
-    <>
-        <Navbar></Navbar>
-        <form className="register reglog">
-            <h1>Register</h1>
-            <p id='em'>Email</p><input type="email" />
-            <p>Username</p><input type="text" />
-            <p>Password</p><input type="password" />
-            <p id='cp'>Confirm Password</p><input type="password" />
-            <br />
-            <input type="submit" value="Register" />
-        </form>
-    </>
-);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            window.location.href = '/todos';
+        } 
+    }, []);
+
+    async function registerUser(e) {
+        e.preventDefault();
+        const response = await fetch('http://localhost:5000/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: document.querySelector('input[type="text"]').value,
+                email: document.querySelector('input[type="email"]').value,
+                password: document.querySelector('input[type="password"]').value
+            })
+        });
+
+        const data = await response.json();
+
+        if(data.user)
+        {
+            window.location.href = '/login';
+        }
+    }
+
+    return ( 
+        <>
+            <Navbar></Navbar>
+            <form onSubmit={registerUser} className="register reglog">
+                <h1>Register</h1>
+                <p id='em'>Email</p><input type="email" />
+                <p>Username</p><input type="text" />
+                <p>Password</p><input type="password" />
+                <p id='cp'>Confirm Password</p><input type="password" />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+        </>
+    );
 }
  
 export default Register;
