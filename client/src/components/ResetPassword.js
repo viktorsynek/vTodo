d16d61cb8058c26a5a75cc21c803652a155e2232
viktorsynek <ktor.syn@gmail.com>
2024-02-29 20:02:43 +0100

    pushed text to left so its aligned

---
+++ client/src/components/ResetPassword.js
@@ -0,0 +1,48 @@
+import Navbar from './Navbar'
+
+const ForgotPassword = () => {
+
+    const forgotPassword = async (e) => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:5000/api/auth/resetpassword', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                password: document.querySelector('input[type="password"]').value,
+            })
+        });
+
+        const data = await response.json();
+        if(data.success){
+            alert('Check your email for a reset link');
+        } else {
+            alert('Email not found');
+        }
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div onSubmit={forgotPassword} className="password">
+                <h1>Reset Password</h1>
+                <div className="container">
+                <form action="" method="post">
+                    <div className="txt_field">
+                        <p>New Password</p>
+                        <input type="password" placeholder=''required/>
+                        <p>Confirm Password</p>
+                        <input type="password" placeholder=''required/>
+                    </div>
+                    <div>
+                        <input type="submit" value={"Send"}/>
+                    </div>
+                </form>
+                </div>
+            </div>
+        </>
+    );
+}
+ 
+export default ForgotPassword;
