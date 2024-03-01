ef271c64d68bb1e058307cca924476cb84b1586f
viktorsynek <ktor.syn@gmail.com>
2024-03-01 13:31:24 +0100

    create sucess message popups

---
+++ client/src/components/SucessMessage.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const SucessMessage = ({ sucessMsg }) => {
+    const [isVisible, setIsVisible] = useState(false); 
+  
+    useEffect(() => {
+        if (sucessMsg)
+            setIsVisible(true); 
+    }, [sucessMsg]);
+  
+    return (
+        <div
+        className={`sucess-box`}
+        style={{ display: isVisible ? 'inline-block' : 'none' }}
+      >
+        <p>{sucessMsg}</p>
+      </div>
+    );
+}
+
+export default SucessMessage;
