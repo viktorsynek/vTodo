d526eca3426fd556067b77e65a73f57e33d7e1fd
viktorsynek <ktor.syn@gmail.com>
2024-03-01 10:43:53 +0100

    add error message popups to handle authentication errors @ login and register

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,11 @@
 import Navbar from './Navbar';
 import '../css/reglog.css';
 import {Link} from 'react-router-dom';
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import ErrorMessage from './ErrorMessage';
 
 const Login = () => {
+    const [errorMsg, setErrorMsg] = useState('');
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
@@ -14,15 +15,18 @@ const Login = () => {
 
     async function loginUser(e) {
         e.preventDefault();
+        const username = e.target.querySelector('input[type="text"]').value;
+        const password = e.target.querySelector('input[type="password"]').value;
+        if (!username || !password) {
+            setErrorMsg('Please provide a username and password');
+            return;
+        }
         const response = await fetch('http://localhost:5000/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                username: document.querySelector('input[type="text"]').value,
-                password: document.querySelector('input[type="password"]').value
-            })
+            body: JSON.stringify({ username, password })
         });
 
         const data = await response.json();
@@ -30,12 +34,15 @@ const Login = () => {
         {
             localStorage.setItem('token', data.token);
             window.location.href = '/todos';
+        } else {
+            setErrorMsg(data.message);
         }
     }
 
     return ( 
     <>
         <Navbar></Navbar>
+        {errorMsg && <ErrorMessage errorMsg={errorMsg} />}
         <form onSubmit={loginUser} className="login reglog">
             <h1>Login</h1>
             <p>Username</p><input type="text" />
