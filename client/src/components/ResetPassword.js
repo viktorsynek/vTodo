37191a35c1470a38e5cc02a4bc35aee3ef922ced
viktorsynek <ktor.syn@gmail.com>
2024-03-01 13:31:40 +0100

    create reset pw logic and implement error handlers

--- client/src/components/ResetPassword.js
+++ client/src/components/ResetPassword.js
@@ -1,30 +1,51 @@
-import Navbar from './Navbar'
+import { useState } from 'react';
+import Navbar from './Navbar';
+import ErrorMessage from './ErrorMessage';
+import SucessMessage from './SucessMessage';
 
 const ResetPassword = () => {
+    const [errorMsg, setErrorMsg] = useState('');
+    const [sucessMsg, setSucessMsg] = useState('');
+
+    const currentUrl = window.location.href;
+    const token = currentUrl.split('=').pop();
 
     const resetPassword = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/api/auth/resetpassword', {
+        setErrorMsg('');
+        setSucessMsg('');
+        const password = e.target.querySelector('input[type="password"]').value;
+        const confirmPassword = e.target.querySelector('input[name="confirm-password"]').value;
+
+        if (!password || !confirmPassword) {
+            setErrorMsg('Please provide a password');
+            return;
+        }
+        if(password !== confirmPassword){
+            setErrorMsg('Passwords do not match');
+        }
+
+        const response = await fetch(`http://localhost:5000/api/auth/resetPassword/${token}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                password: document.querySelector('input[type="password"]').value,
-            })
+            body: JSON.stringify({password:password, token: token})
         });
 
         const data = await response.json();
         if(data.success){
-            alert('Check your email for a reset link');
+            setSucessMsg('Password reset successful!');
         } else {
-            alert('Email not found');
+            setErrorMsg(data.message);
         }
     }
 
     return (
         <>
             <Navbar />
+            {errorMsg && <ErrorMessage errorMsg={errorMsg} />}
+            {sucessMsg && <SucessMessage sucessMsg={sucessMsg} />}
             <div onSubmit={resetPassword} className="password">
                 <h1>Reset Password</h1>
                 <div className="container">
@@ -33,7 +54,7 @@ const ResetPassword = () => {
                         <p id='np'>New Password</p>
                         <input type="password" placeholder=''required/>
                         <p id='cp'>Confirm Password</p>
-                        <input type="password" placeholder=''required/>
+                        <input type="password" placeholder='' name='confirm-password' required/>
                     </div>
                     <div>
                         <input type="submit" value={"Send"}/>
