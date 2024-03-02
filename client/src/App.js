cff426913c6c1cc0df22ecba7c5946834392cf2b
viktorsynek <ktor.syn@gmail.com>
2024-03-02 21:41:29 +0100

    add create-todo route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './css/main.css';
+import './css/todo.css';
 import PageNotFound from './components/404';
 import Login from './components/Login';
 import Todos from './components/Todos';
 import Register from './components/Register';
 import ForgotPassword from './components/Password';
 import ResetPassword from './components/ResetPassword';
+import CreateTodo from './components/CreateTodo';
 import {Route, Routes, useNavigate} from 'react-router-dom';
 import React from 'react';
 
@@ -29,6 +31,7 @@ function App() {
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/resetpassword" element={<ResetPassword />} />
           <Route path="/todos" element={<Todos />} />
+          <Route path="/create-todo" element={<CreateTodo />} />
       </Routes>
     </>
   );
