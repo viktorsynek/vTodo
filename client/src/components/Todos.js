bd4a9258ce3bf25977693f34f4698d017233daaf
viktorsynek <ktor.syn@gmail.com>
2024-03-02 21:42:23 +0100

    readded search bar + some prototype functions

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faPen, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -21,60 +21,37 @@ const Todos = () => {
         return null;
     }
 
+    const searchTodo = async (e) => {
+        e.preventDefault();
+        const search = document.getElementById('search').value;
+        console.log(`searching todo named: ${search}`);
+    }
+
+    const createTodo = async (e) => {
+        e.preventDefault();
+        window.location.href = '/create-todo';
+    }
+
+
     return ( 
     <>
         <Navbar></Navbar>
         <div className='box'>
             <div className='search-box'>
-                <button id='add'>Add Task</button>
+                <button onClick={createTodo} id='add'>Add Task</button>
+                <input id='search' type="text" name='search' placeholder='Search by title . . .'/>
+                <button onClick={searchTodo} id='search-btn'>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
             </div>
             <div className='todos'>
-                <div className="todo todo1">
+                <div className="todo">
                     <div>
                         <input type="checkbox" name='check'/>
                     </div>
                     <div className='left'>
                         <h3>Create a react project 🔥</h3>
                         <p>2024/02/20 5:23AM</p>
-                    </div>
-                    <div className='right'>
-                        <button><FontAwesomeIcon icon={faPen} /></button>
-                        <button><FontAwesomeIcon icon={faTrash} /></button>
-                    </div>
-                </div>
-                <div className="todo todo2">
-                <div>
-                    <input type="checkbox" name='check'/>
-                    </div>
-                    <div className='left'>
-                        <h3>Create a C++ project 👩‍💻</h3>
-                        <p>2024/02/20 11:33PM</p>
-                    </div>
-                    <div className='right'>
-                        <button><FontAwesomeIcon icon={faPen} /></button>
-                        <button><FontAwesomeIcon icon={faTrash} /></button>
-                    </div>
-                </div>
-                <div className="todo todo2">
-                <div>
-                    <input type="checkbox" name='check'/>
-                    </div>
-                    <div className='left'>
-                        <h3>Create a Python project 🐍</h3>
-                        <p>2024/02/20 11:41PM</p>
-                    </div>
-                    <div className='right'>
-                        <button><FontAwesomeIcon icon={faPen} /></button>
-                        <button><FontAwesomeIcon icon={faTrash} /></button>
-                    </div>
-                </div>
-                <div className="todo todo2">
-                <div>
-                    <input type="checkbox" name='check'/>
-                    </div>
-                    <div className='left'>
-                        <h3>Create a Java project ☕</h3>
-                        <p>2024/02/20 11:49PM</p>
                     </div>
                     <div className='right'>
                         <button><FontAwesomeIcon icon={faPen} /></button>
