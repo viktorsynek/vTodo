18ccaf90f0deb429ee4365743afc31706c43220a
viktorsynek <ktor.syn@gmail.com>
2024-03-06 21:36:49 +0100

    remove token logging for testing

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 exports.getTodos = async (req, res, next) => {
 	try {
 		const token = req.headers.authorization;
-		console.log(token);
 		if (!token) {
 			return res
 				.status(401)
