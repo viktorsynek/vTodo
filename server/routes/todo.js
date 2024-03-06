cdcea199264609f5cdf9013f04ec0c40ed1b1204
viktorsynek <ktor.syn@gmail.com>
2024-03-06 21:34:57 +0100

    remove protect middleware from todos

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -6,14 +6,9 @@ const {
 	createTodo,
 	deleteTodo,
 } = require("../controllers/todos");
-const { protect } = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/").get(protect, getTodos).post(protect, createTodo);
-router
-	.route("/:id")
-	.get(protect, getTodo)
-	.put(protect, updateTodo)
-	.delete(protect, deleteTodo);
+router.route("/").get(getTodos).post(createTodo);
+router.route("/:id").get(getTodo).put(updateTodo).delete(deleteTodo);
 
 module.exports = router;
