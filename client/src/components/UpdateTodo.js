4c457662edd5bf4258cff24925da1593211b11f3
viktorsynek <ktor.syn@gmail.com>
2024-03-07 19:29:08 +0100

    create update todo page

---
+++ client/src/components/UpdateTodo.js
@@ -0,0 +1,98 @@
+import Navbar from "./Navbar";
+import ErrorMessage from "./ErrorMessage";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+
+const UpdateTodo = () => {
+	const { todoId } = useParams();
+
+	const [errorMsg, setErrorMsg] = useState("");
+	const [todoData, setTodoData] = useState({
+		title: "",
+		description: "",
+	});
+
+	useEffect(() => {
+		const fetchTodoDetails = async () => {
+			try {
+				const response = await fetch(
+					`http://localhost:5000/api/todos/${todoId}`,
+					{
+						method: "GET",
+						headers: {
+							Authorization: localStorage.getItem("token"),
+							"Content-Type": "application/json",
+						},
+					}
+				);
+				const data = await response.json();
+				if (response.ok) {
+					setTodoData({
+						title: data.data.title,
+						description: data.data.description,
+					});
+				} else {
+					setErrorMsg(data.message);
+				}
+			} catch (error) {
+				console.error("Error fetching todo details:", error);
+			}
+		};
+
+		fetchTodoDetails();
+	}, [todoId]);
+
+	const updateTodo = async (e) => {
+		e.preventDefault();
+		const title = document.getElementById("todo-title").value;
+		const desc = document.getElementById("desc").value;
+		const response = await fetch(`http://localhost:5000/api/todos/${todoId}`, {
+			method: "PUT",
+			headers: {
+				Authorization: localStorage.getItem("token"),
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ title, description: desc }),
+		});
+		const data = await response.json();
+		if (data.success) {
+			window.location.href = "/todos";
+		} else {
+			setErrorMsg(data.message);
+		}
+	};
+
+	return (
+		<>
+			<Navbar></Navbar>
+			{errorMsg && <ErrorMessage errorMsg={errorMsg} />}
+			<div className="update">
+				<h1>Update Todo</h1>
+				<p>Title</p>
+				<input
+					type="text"
+					name="title"
+					id="todo-title"
+					value={todoData.title}
+					onChange={(e) => setTodoData({ ...todoData, title: e.target.value })}
+				/>
+				<br />
+				<textarea
+					name="desc"
+					id="desc"
+					cols="30"
+					rows="13"
+					value={todoData.description}
+					onChange={(e) =>
+						setTodoData({ ...todoData, description: e.target.value })
+					}
+				></textarea>
+				<br />
+				<button onClick={updateTodo}>Update Todo</button>
+				<Link to="/todos">Go Back Home</Link>
+			</div>
+		</>
+	);
+};
+
+export default UpdateTodo;
