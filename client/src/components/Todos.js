e39a0a592ebf7a03a39c9b53c18ad6e88e7be487
viktorsynek <ktor.syn@gmail.com>
2024-03-07 19:28:59 +0100

    add fetchTodos for all REST methods

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faTrash,
-	faPen,
-	faMagnifyingGlass,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPen, faEye } from "@fortawesome/free-solid-svg-icons";
 
 const Todos = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -19,7 +15,7 @@ const Todos = () => {
 		}
 		fetchTodos();
 		setIsLoggedIn(true);
-	}, []);
+	}, [isLoggedIn]);
 
 	const fetchTodos = async () => {
 		const response = await fetch("http://localhost:5000/api/todos/", {
@@ -30,22 +26,45 @@ const Todos = () => {
 			},
 		});
 		const data = await response.json();
-		console.log(data);
 		setTodos(data.data);
-		if (!data.sucess) {
-			console.log(data.message);
+		if (!data.success) {
+			console.log(data.error);
 		}
-	};
-
-	const searchTodo = async (e) => {
-		e.preventDefault();
-		const search = document.getElementById("search").value;
-		console.log(`searching todo named: ${search}`);
 	};
 
 	const createTodo = async (e) => {
 		e.preventDefault();
 		window.location.href = "/create-todo";
+	};
+
+	const deleteTodo = async (todoId) => {
+		try {
+			const response = await fetch(
+				`http://localhost:5000/api/todos/${todoId}`,
+				{
+					method: "DELETE",
+					headers: {
+						Authorization: localStorage.getItem("token"),
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			if (response.ok) {
+				fetchTodos();
+			} else {
+				console.error("Failed to delete todo");
+			}
+		} catch (error) {
+			console.error("Error deleting todo:", error);
+		}
+	};
+
+	const checkTodo = (todoId) => {
+		window.location.href = `/todos/${todoId}`;
+	};
+
+	const updateTodo = (todoId) => {
+		window.location.href = `/todos/update/${todoId}`;
 	};
 
 	return (
@@ -56,36 +75,34 @@ const Todos = () => {
 					<button onClick={createTodo} id="add">
 						Add Task
 					</button>
-					<input
-						id="search"
-						type="text"
-						name="search"
-						placeholder="Search by title . . ."
-					/>
-					<button onClick={searchTodo} id="search-btn">
-						<FontAwesomeIcon icon={faMagnifyingGlass} />
-					</button>
 				</div>
 				<div className="todos">
-					{todos.map((todo) => (
-						<div key={todo._id} className="todo">
-							<div>
-								<input type="checkbox" name="check" />
+					{todos.length === 0 ? (
+						<p id="none">No todos available {":("}</p>
+					) : (
+						todos.map((todo) => (
+							<div key={todo._id} className="todo">
+								<div>
+									<input type="checkbox" name="check" />
+								</div>
+								<div className="left">
+									<h3>{todo.title}</h3>
+									<p>{new Date(todo.date).toLocaleString()}</p>
+								</div>
+								<div className="right">
+									<button onClick={() => checkTodo(todo._id)}>
+										<FontAwesomeIcon icon={faEye} />
+									</button>
+									<button onClick={() => updateTodo(todo._id)}>
+										<FontAwesomeIcon icon={faPen} />
+									</button>
+									<button onClick={() => deleteTodo(todo._id)}>
+										<FontAwesomeIcon icon={faTrash} />
+									</button>
+								</div>
 							</div>
-							<div className="left">
-								<h3>{todo.title}</h3>
-								<p>{new Date(todo.date).toLocaleString()}</p>
-							</div>
-							<div className="right">
-								<button>
-									<FontAwesomeIcon icon={faPen} />
-								</button>
-								<button>
-									<FontAwesomeIcon icon={faTrash} />
-								</button>
-							</div>
-						</div>
-					))}
+						))
+					)}
 				</div>
 			</div>
 		</>
