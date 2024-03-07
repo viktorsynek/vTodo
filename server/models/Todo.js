79a21b13e34ff93059639275d6b6a38a229f9f40
viktorsynek <ktor.syn@gmail.com>
2024-03-07 19:30:06 +0100

    change title maxlength 100 -> 30

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -4,7 +4,7 @@ const TodoSchema = new mongoose.Schema({
 	title: {
 		type: String,
 		required: [true, "Please fill out the title of the todo."],
-		maxlength: [100, "Title can not be more than 50 characters"],
+		maxlength: [30, "Title can not be more than 50 characters"],
 	},
 	description: {
 		type: String,
